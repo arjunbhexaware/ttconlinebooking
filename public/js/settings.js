91cd8fa4e4cba7c58c99fea7a1e1c8c044c91063
Your Name <you@example.com>
2018-04-24 12:14:22 +0530

    first commit

--- public/js/settings.js
+++ public/js/settings.js
@@ -8,8 +8,8 @@ This file is part of the Innovation LAB - Offline Bot.
 
 define({    
 	eventAllow :["Yes","No","Italian Holiday Summer 2018","Rome","London","Paris","Amsterdam","Yes, please", "Overview","No, thanks", "Italy","Europe","Tour Packages","Wonders of Italy *NEW* Summer 2018","May ' 18","June ' 18","July ' 18","August ' 18","September ' 18","October ' 18","November ' 18","December ' 18","8 - 14 days (50)","14 + days (27)","1 - 7 days (3)","US$0 - $1000(1)","US$1001 - $1500(3)","US$1501 - $2000(13)","US$2001 - $2500(12)","US$2501 - $3000(10)","US$3001 - $5000(27)","US$5001 - $10000(12)","About this Trip","Dates & Prices","Your Itinerary","Book Date","What's included","Trafalgar Experience","Dates & Prices","Guest Photos","Easy Quote","1","2","3","4","5","6","7","8","9","10","03-08-2018","10-08-2018","17-08-2018","24-08-2018","31-08-2018","Twin Share Room US$1,850.00","book my own flights"],
-    //chatServerURL : "http://localhost:3000/dialogflowAPI",
-	chatServerURL : "https://ttcb2cob.herokuapp.com/dialogflowAPI",
+    chatServerURL : "./dialogflowAPI",
+	//chatServerURL : "https://ttcb2cob.herokuapp.com/dialogflowAPI",
 	accessToken:"123445",
     applicationTitle: "TTCB2C",
     userTitle : "Me",
