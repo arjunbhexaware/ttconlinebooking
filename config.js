acd5db067c6185db7d4bba3771ff4042a9bd7283
arjunbhexaware <37321509+arjunbhexaware@users.noreply.github.com>
2018-04-25 10:13:14 +0530

    Mail format update
    Update by 40697

--- config.js
+++ config.js
@@ -1,35 +1,38 @@
 module.exports = {	
 	accessToken: "9beeae3c7763480faefb5abf9e1ba1e6",
     dialogflowAPI : "https://api.api.ai/v1/query?v=20150910",
-	mailBody:	`<pre>
+	mailBody:	`
+<div style="margin-bottom:10px">Greetings John,</div>
+<div style="margin-bottom:10px"> 
+Thank You very much for booking on Trafalgar & for giving us the opportunity to serve you. We will do our best to be sure we are ready for your travel, provide the best of experiences during the tour, all the amenities & luxuries we can and strive to provide you with our world class standards for exceptional service.
+ </div>
+ <div>
+To complete the booking process & serve you better though, we will need the following additional details from your end:
+<ul>
+  <li>  Nationality</li>
+  <li> Complete Passport details</li>
+  <li> Proper Insurance Details</li>
+</ul>
+                  
+ </div>
+<div style="margin-bottom:10px">Request you to kindly capture the related details through this URL link –</div>
+ 
+<a href="https://www.trafalgar.com/gsa/complete-your-booking-details" style="margin-bottom:10px">https://www.trafalgar.com/gsa/complete-your-booking-details</a>
+ 
+ <div style="margin-bottom:10px;margin-top:15px">
+Should you have any difficulties with the same, please email us immediately so that we can assist you on the same. If you have any questions, please call us or use the contact page for further help!
+ </div>
+ <div style="margin-bottom:15px">
+Again, Thank you for booking with Trafalgar and we look forward to serving you. As always, You are at the center of everything we do, and we will work tirelessly towards ensuring that we meet and exceed your expectations in every possible way.
+ </div>
+ <div>
+<div>With Kindest Wishes & Warm Regards,</div>
 
-				Greetings toName,
-				Booking Id : orderId
-				Thank You very much for booking on Trafalgar & for giving us the opportunity to serve you. We will do our best to be sure we are ready for your travel, provide the best of experiences during the tour, all the amenities & luxuries we can and strive to provide you with our world class standards for exceptional service.
-
-				To complete the booking process & serve you better though, we will need the following additional details from your end:
-						•	Name
-						•	Surname
-						•	mail
-						•	contact no
-						•	Nationality
-						•	Complete Passport details
-						•	Proper Insurance Details
-
-				Request you to kindly capture the related details through this URL link –
-
-				https://www.trafalgar.com/gsa/complete-your-booking-details
-
-				Should you have any difficulties with the same, please email us immediately so that we can assist you on the same. If you have any questions, please call us or use the contact page for further help!
-
-				Again, Thank you for booking with Trafalgar and we look forward to serving you. As always, You are at the center of everything we do, and we will work tirelessly towards ensuring that we meet and exceed your expectations in every possible way.
-
-				With Kindest Wishes & Warm Regards,
-				Your sincerely,
-				Peter Pan,
-				Trafalgar Tour Director
-
-				</pre>`
+<div>Your sincerely,</div>
+<div>Peter Pan,</div>
+<div>Trafalgar Tour Director</div>
+</div>
+`
 }
 
 
